d37bd11b5121d94acdac7cf0d210ba9ae663ced4
Maxim Imakaev <mimakaev@gmail.com>
2012-03-31 18:04:46 -0400

    Automatic commit from a new build

--- doc/build.sh
+++ doc/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sphinx-build . _build
-cp -r  _build/*  ../manual_pages/mimakaev.bitbucket.org/iterative_correction_paper_f7f91c861335
-cd ../manual_pages/mimakaev.bitbucket.org
+cp -r  _build/*  ../../manual_pages/mimakaev.bitbucket.org/iterative_correction_paper_f7f91c861335
 cd ../manual_pages/mimakaev.bitbucket.org
 hg add * 
 hg commit -m "Automatic commit from a new build" 
