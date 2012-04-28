9b14047f39369d4f2061a8ec1247b40b5eb025c3
Maxim Imakaev <mimakaev@gmail.com>
2012-04-27 21:20:49 -0400

    Build script changed

--- doc/build.sh
+++ doc/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sphinx-build . _build
-cp -r  _build/*  ../../manual_pages/mimakaev.bitbucket.org/iterative_correction_paper_f7f91c861335
+make html 
+rm -r ../../manual_pages/mimakaev.bitbucket.org/iterative_correction_paper_f7f91c861335
+mv  build/html  ../../manual_pages/mimakaev.bitbucket.org/iterative_correction_paper_f7f91c861335
+rm -r build
 cd ../../manual_pages/mimakaev.bitbucket.org
 hg add * 
 hg commit -m "Automatic commit from a new build" 
