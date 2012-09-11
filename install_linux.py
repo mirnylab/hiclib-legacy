ee7cf6b6eacfc319d9441be701e3a312b026f637
Maxim Imakaev <mimakaev@gmail.com>
2012-09-11 12:45:42 -0400

    Some checks added for install scipt

--- install_linux.py
+++ install_linux.py
@@ -2,6 +2,69 @@
 '''
 import os
 import sys
+
+print
+print "To skip checks use python install_linux.py --noCheck"
+print
+
+if (len(sys.argv) <= 1) or  (sys.argv[1].lower()[-7:] != "nocheck"):
+    print "Checking python version..",
+    ver = sys.version_info
+    if (ver < (2, 6)) or (ver >= (3, 0)):
+        raise RuntimeError("Please use python 2.6+, but not 3.x")
+    print 'Correct!'
+
+    print "Checking for mirnylib..",
+    try:
+        import mirnylib #@UnusedImport @IgnorePep8
+    except:
+        print "Mirnylib library not installed"
+        print "see http://bitbucket.org/mirnylab/mirnylib"
+        raise RuntimeError("Mirnylib library not installed")
+    print "Found!"
+
+    print "Checking for numpy..",
+    try:
+        import numpy
+        print "Found!"
+    except:
+        print "Numpy not found"
+        raise RuntimeError("Numpy not found")
+
+    print "Checking for numpy version..",
+    try:
+        nv = numpy.__version__
+        nums = [int(i) for i in nv.split(".")]
+        nums = tuple(nums[:2])
+        assert nums >= (1, 6)
+        print "Correct!"
+    except:
+        print "numpy version is %s" % nv
+        print "Needs at least numpy 1.6"
+        print "See manual for numpy installation guide"
+        raise RuntimeError("Wrong numpy version")
+
+    print "Checking for mirnylib.h5dict install..",
+    from mirnylib.h5dict import h5dict
+    a = h5dict()
+    b = numpy.empty(1000000, dtype="int16")
+    c = "bla bla bla"
+    a["numpy"] = b
+    a["object"] = c
+    assert (a["numpy"] - b).sum() == 0
+    print "H5dict test successful!"
+
+    print "Checking for joblib..",
+    try:
+        import joblib
+        print "Found!"
+    except:
+        print "joblib not found"
+        raise RuntimeError("joblib not found")
+
+    print
+
+
 os.chdir("src")
 libpath = os.getcwd()
 libpath = os.path.normpath(libpath.replace(os.path.expanduser('~'), '$HOME/'))
