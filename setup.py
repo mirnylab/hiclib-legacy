2756516380b419c2a637866f83863c7d5f071b1c
Nezar Abdennur <nabdennur@gmail.com>
2014-01-22 00:06:12 -0500

    changed dir structure and add simple setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='hiclib',
+    url='http://mirnylab.bitbucket.org/hiclib/index.html',
+    description=('Hi-C data analysis library.'),
+    packages=['hiclib'],
+)
