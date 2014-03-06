9ad6389319efcdce1a894d141bed69289f837d8f
Nezar Abdennur <nabdennur@gmail.com>
2014-03-06 02:44:12 -0500

    Switch to setuptools and declaring install dependencies, added README.md

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='hiclib',
     url='http://mirnylab.bitbucket.org/hiclib/index.html',
     description=('Hi-C data analysis library.'),
     packages=['hiclib'],
+    install_requires=[
+        'numpy>=1.6',
+        'scipy',
+        'matplotlib',
+        'biopython',
+        'mirnylib',
+        'pysam',
+    ],
+    dependency_links=[
+        'https://bitbucket.org/mirnylab/mirnylib/get/tip.tar.bz2'
+    ],
 )
