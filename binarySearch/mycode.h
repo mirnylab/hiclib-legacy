f951d201448745d59f73b41daca319f69f7bbd25
Maxim Imakaev <mimakaev@gmail.com>
2014-12-22 19:14:53 -0500

    Added fast binary search
    --HG--
    branch : test-mapping

---
+++ binarySearch/mycode.h
@@ -0,0 +1,8 @@
+#ifndef __MYCODE_H__
+#define __MYCODE_H__
+#include <cstdint>
+void int64BinarySearch(uint64_t* data, long int N, uint64_t* keys, long int M, int32_t* result, uint64_t mult);
+//void uint64BinarySearch(uint64_t* data, long int N, uint64_t* keys, long int M, int32_t* result, uint64_t multiplyer)
+#endif
+
+
