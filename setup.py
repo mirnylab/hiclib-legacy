80391802eab52486280b52ec7ce697a43ce43d61
nvictus <nezar@mit.edu>
2015-09-30 00:11:19 +0000

    requires legacy pysam for now

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'biopython',
         'mirnylib',
-        'pysam',
+        'pysam <=0.6',
     ],
     dependency_links=[
         'https://bitbucket.org/mirnylab/mirnylib/get/tip.tar.bz2'
